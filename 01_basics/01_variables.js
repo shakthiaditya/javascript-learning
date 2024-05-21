3db1d41b3d1cc4c27bcd4f30d28378169f37653e
Shakthi Aditya M <msadit2003@gmail.com>
2024-05-21 21:17:46 +0000

    data types and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553;
+let accountEmail = "msadit2003@gmail.com";
+var accountPassword = "12345";
+accountCity = "Chennai";
+let accountState;
+
+// accountId = 5;
+accountEmail = "hello@hello.com";
+accountPassword = 741852;
+accountCity = "Bengaluru";
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+//Prints values of the mentioned variables in a table format
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,6 @@
+"use strict"    //Treat all the code like new version of javascript
+
+// alert(4+6);
+
+let confirmation = null;
+console.log(typeof(confirmation));

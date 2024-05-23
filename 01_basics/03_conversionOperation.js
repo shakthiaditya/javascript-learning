7dc9ad11e9219b828eec4ae51cd1e850bece388d
Shakthi Aditya M <msadit2003@gmail.com>
2024-05-23 11:37:25 +0000

    Learning datatype conversions in javascript

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = undefined;
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+let isLoggedIn = "";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)
+
+
